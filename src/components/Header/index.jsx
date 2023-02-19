2521de765cc05df99bacf66a19324cc715ded737
Lubni Morais <lubni.morais@gmail.com>
2023-02-19 18:51:49 -0300

    First commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <strong>Ignite Feed</strong>
+    </header>
+  )
+
+}
+
+export { Header }
